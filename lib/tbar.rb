9b1a4c820c2272fd15e591c3cc8d270ca752b4cb
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-03-11 16:44:56 -0600

    make it known we're doing kwargs

--- lib/tbar.rb
+++ lib/tbar.rb
@@ -1,3 +1,9 @@
 module Tbar
   VERSION = "1.0.0"
 end
+require 'tbar/errors'
+require 'tbar/account_type'
+require 'tbar/entry'
+require 'tbar/credit'
+require 'tbar/debit'
+require 'tbar/transaction'
--- lib/tbar/entry.rb
+++ lib/tbar/entry.rb
@@ -1,14 +1,14 @@
 module Tbar
   # Private: Entry is one item of a Transaction. It represents one of the
-  # Credit or Debit entries in a Transaction. This class is 
+  # Credit or Debit entries in a Transaction.
   class Entry
     # The Account this posting is for
     attr_reader :account
 
-    # The Amount of the Posting
+    # The Amount of the Posting, this must be a whole number
     attr_reader :amount
 
-    # The Commodity - USD, Stock, etc.
+    # The Commodity - USD, ticker symbol, etc.
     attr_reader :commodity
 
     # Additional text to go with this posting
@@ -22,12 +22,12 @@ module Tbar
       @default_commodity = com
     end
 
-    def initialize( args = {} )
-      @account   = args.fetch( :account )
-      @amount    = args.fetch( :amount )
+    def initialize( kwargs = {} )
+      @account   = kwargs.fetch( :account )
+      @amount    = kwargs.fetch( :amount )
 
-      @commodity = args.fetch( :commodity, self.class.default_commodity )
-      @note      = args.fetch( :note, nil )
+      @commodity = kwargs.fetch( :commodity, self.class.default_commodity )
+      @note      = kwargs.fetch( :note, nil )
     end
 
     def type
