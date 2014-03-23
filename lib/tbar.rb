688328e188f6db0d8f9aa88a285f0363ad869e29
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-03-22 19:11:44 -0600

    add all the files to the top level require

--- lib/tbar.rb
+++ lib/tbar.rb
@@ -3,7 +3,10 @@ module Tbar
 end
 require 'tbar/errors'
 require 'tbar/account_type'
+require 'tbar/account'
+require 'tbar/account_group'
 require 'tbar/entry'
 require 'tbar/credit'
 require 'tbar/debit'
 require 'tbar/transaction'
+require 'tbar/chart_of_accounts'
