7874cd30c26a2e4a04d9702de85a9ee06d3c3e3a
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-03-22 18:00:03 -0600

    do not allow creating children to an account group
    This make sure that account group type is not 
    propagated to its children

--- lib/tbar/account_group.rb
+++ lib/tbar/account_group.rb
@@ -4,12 +4,19 @@ module Tbar
   #
   # The Account group does not count as part of the depth, or size of the tree.
   # It is an invisible node.
+  #
+  # You cannot create direct children to an Account Group, you can only attach
+  # Accounts to it or use add_child.
   class AccountGroup < Account
     # Public: Create a new Account Group
     #
     #
     def initialize( kwargs = {} )
       super( kwargs.merge( :type => Tbar::AccountType::GROUP ) )
+    end
+
+    def create_child( name )
+      raise NotImplementedError, "Unable to create a child to an AccountGroup, please use add_child(String) or attach_child(Account)"
     end
 
     protected
