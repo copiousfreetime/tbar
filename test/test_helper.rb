5ad649e5ac837976b22943fc1fac8aa1196aeab6
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-03-22 18:33:36 -0600

    add test data file access

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,20 @@ require 'simplecov'
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pathname'
+
+module Tbar
+  class Test < ::Minitest::Test
+    def directory
+      Pathname.new( __FILE__ ).dirname
+    end
+
+    def data_directory
+      directory.join( 'data' )
+    end
+
+    def data_file( basename )
+      data_directory.join( basename )
+    end
+  end
+end
