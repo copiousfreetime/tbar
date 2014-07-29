0953f60e504e5ad1ce3945c3e89b49760b8472d2
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-07-28 21:30:42 -0600

    serial data types.

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,32 @@
 -- Payee table
 
 CREATE TABLE IF NOT EXISTS accounts(
-  id   integer PRIMARY KEY,
+  id   SERIAL PRIMARY KEY,
   name text    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS payees(
-  id          integer PRIMARY KEY,
+  id          SERIAL PRIMARY KEY,
   account_id  integer NOT NULL REFERENCES accounts(id),
   name        text    NOT NULL
 );
 
 -- Used for the jaro winkler testing
 CREATE TABLE IF NOT EXISTS to_payees(
-  id          integer PRIMARY KEY,
+  id          SERIAL PRIMARY KEY,
   payee_id    integer NOT NULL REFERENCES payees(id),
   description text    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS transactions (
-  id        integer PRIMARY KEY,
+  id        SERIAL PRIMARY KEY,
   payee_id  integer NOT NULL REFERENCES payees(id),
   date      date    NOT NULL,
   note      text
 );
 
 CREATE TABLE IF NOT EXISTS entries (
-  id              integer PRIMARY KEY,
+  id              SERIAL PRIMARY KEY,
   account_id      integer NOT NULL REFERENCES accounts(id),
   transaction_id  integer NOT NULL REFERENCES transactions(id),
   comodity        text    NOT NULL DEFAULT 'USD',
