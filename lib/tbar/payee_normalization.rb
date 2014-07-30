9afd2dd57c61fdcfd182522d858051946b1e55f4
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-07-29 20:14:50 -0600

    normalizing payee’s and accounts

--- lib/tbar/payee_normalization.rb
+++ lib/tbar/payee_normalization.rb
@@ -3,16 +3,45 @@ require 'highline/import'
 module Tbar
   class PayeeNormalization
     attr_reader :db
+    attr_reader :chart
     def initialize( db )
       @db = db
       @jw = JaroWinkler.new
+      @chart = load_chart_of_accounts
+      $terminal.wrap_at = 160
     end
 
     def call
       notes_to_payees
+      associate_payees_to_accounts
     end
 
     private
+
+    def load_chart_of_accounts
+      Tbar::ChartOfAccounts.new.tap do |chart|
+        db[:accounts].select(:name).order_by(:name).each do |row|
+          chart.add_account( row[:name] )
+        end
+      end
+    end
+
+    def associate_payees_to_accounts
+      db[:payees].where( :associated => true, :account_id => nil ).each do |row|
+        choose do |menu|
+          menu.index  = :number
+          menu.prompt = "What Account should be the default associated withe #{row[:name]}"
+          by_name     = chart.accounts_by_name
+          menu.choices( *by_name.keys.sort.map { |s| s.strip } ) do |name|
+            account    = by_name[name]
+            account_id = db[:accounts][:name => name][:id]
+            puts "Setting #{row[:name]} default account to be #{account_id} (#{name}) #{row.class}"
+            db[:payees].where( :id => row[:id] ).update( :account_id => account_id )
+          end
+          menu.flow   = :columns_down
+        end
+      end
+    end
 
     def notes_to_payees
       db[:import_rows].where( :converted => false ).each do |row|
