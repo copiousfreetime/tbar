a15336f7ce7acb61d044cd93fa1cf057652e1b6c
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-07-30 09:52:30 -0600

    have the ability to change accounts on transaction

--- lib/tbar/process_imports.rb
+++ lib/tbar/process_imports.rb
@@ -33,41 +33,41 @@ module Tbar
 
     def build_transactions
       imports.each_with_index do |import,import_idx|
-        row_count = import[:count]
-        puts "Importing rows from #{import[:path]}"
+        puts "Importing associated rows from #{import[:path]}"
         import_rows( import ).each_with_index do |row,row_idx|
           db.transaction do
             payee   = db[:payees][ :id => row[:payee_id] ]
             if payee[:associated] then
               my_account    = account_for_name( import[:account_name] )
               other_account = db[:accounts][ :id => payee[:account_id] ]
-              their_account = account_for_name( other_account[:name] )
 
+              # Prompt for confirmation on this account and setup a menu if it
+              # isn't
               my_entry    = my_account.entry_for( Monetize.parse(row[:amount]) )
-              their_entry = my_entry.paired_entry( their_account, row[:note] )
+              puts my_entry
+              puts "  #{row[:note]}"
+              their_account_name = if agree("  Paired Account is: #{other_account[:name]}  ok? ") then
+                                     other_account[:name]
+                                   else
+                                     account_prompt( "Which Account should be on the other side?" )
+                                   end
 
+              their_account = account_for_name( their_account_name )
+              their_entry    = my_entry.paired_entry( their_account, row[:note] )
               txn = Transaction.new( :entries => [my_entry, their_entry],
                                      :date    => Date.parse( row[:date] ),
                                      :payee   => payee[:name] )
 
-              puts txn.to_s
               if txn.valid? then
-                if agree( "Does this transaction look good? [#{row_idx+1}/#{row_count}]" ) then
-                  txn_id = db[:transactions].insert( :payee_id => payee[:id], :note => txn.note, :date => txn.date )
-                  txn.entries.each do |entry|
-                    account_id = db[:accounts][ :name => entry.account.real_name ][:id]
-                    entry_id   = db[:entries].insert( :transaction_id => txn_id, :account_id => account_id,
-                                         :amount => entry.amount.cents, :note => entry.note,
-                                         :type   => entry.type.to_s )
-                    db[:import_rows].where( :id => row[:row_id] ).update( :entry_id => entry_id )
-                  end
+                puts txn.to_s
+                if agree( "Does this transaction look good? " ) then
+                  save_txn_to_db( txn, payee, row )
                 else
-                  puts "Okay, ignoring it for now"
+                  puts "Okay, we'll come back to it later"
                 end
               else
                 puts "Transaction is not valid!"
               end
-
             else
               #puts "Skipping #{row.inspect} for now"
               next
@@ -78,6 +78,33 @@ module Tbar
     end
 
     private
+
+
+    def account_prompt( prompt )
+      new_name = nil
+      choose do |menu|
+        menu.index  = :number
+        menu.prompt = prompt
+        by_name     = chart.accounts_by_name
+        menu.choices( *by_name.keys.sort.map { |s| s.strip } ) do |name|
+          puts "Using #{name} instead"
+          new_name = name
+        end
+       menu.flow   = :columns_down
+      end
+      return new_name
+    end
+
+    def save_txn_to_db( txn, payee, row )
+      txn_id = db[:transactions].insert( :payee_id => payee[:id], :note => txn.note, :date => txn.date )
+      txn.entries.each do |entry|
+        account_id = db[:accounts][ :name => entry.account.real_name ][:id]
+        entry_id   = db[:entries].insert( :transaction_id => txn_id, :account_id => account_id,
+                               :amount => entry.amount.cents, :note => entry.note,
+                               :type   => entry.type.to_s )
+        db[:import_rows].where( :id => row[:row_id] ).update( :entry_id => entry_id )
+      end
+    end
 
     def account_for_name( name )
       chart.accounts_by_name[name]
